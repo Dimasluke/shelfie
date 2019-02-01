6ee1a648dd4c2997c0f898d5f57dc010234d5ca7
Luke Dimas <dimasluke@gmail.com>
2019-02-01 10:04:40 -0700

    inital src set done with some progress on server setup

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json())
+
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
